da7160d5de7163761a1043cabc96249d9ee01e62
Garrido Neto <134981206+netogarridosax@users.noreply.github.com>
2024-03-21 23:37:48 -0300

    passport
    correção de rotas

--- app/app.js
+++ app/app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 
+// Importar os controladores
 const PlacaController = require('./controllers/PlacaController');
 const EstaticoController = require('./controllers/EstaticoController');
 const AutorController = require('./controllers/AutorController');
 const AuthController = require('./controllers/AuthController');
-const PlacaSequelizeDao = require('./lib/placa/PlacaSequelizeDao');
-const PlacaDao = require('./lib/placa/PlacaDao');
-const bodyParser = require('body-parser');
-const Sequelize = require('sequelize');
-const passport = require('passport');
-const JwtStrategy = require('passport-jwt').Strategy,
-    ExtractJwt = require('passport-jwt').ExtractJwt;
 
+// Importar DAOs e outros módulos necessários
+const PlacaSequelizeDao = require('./lib/placa/PlacaSequelizeDao');
+
+// Configuração do Passport.js
 let opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.SEGREDO_JWT;
@@ -22,8 +25,7 @@ passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     return done(null, jwt_payload);
 }));
 
-
-
+// Configuração do Sequelize
 const sequelize = new Sequelize(
   process.env.MARIADB_DATABASE,
   'root',
@@ -34,53 +36,44 @@ const sequelize = new Sequelize(
   }
 );
 
+// Setar a view engine
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
+// Instanciar controladores e outros módulos
 let placaDao = new PlacaSequelizeDao(sequelize);
 let placaController = new PlacaController(placaDao);
 let estaticoController = new EstaticoController();
 let autorController = new AutorController();
 let authController = new AuthController(placaDao);
 
-app.use(express.static('public'))
+// Middlewares
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Middleware para registrar informações de solicitação
 app.use((req, res, next) => {
   const currentDate = new Date().toLocaleDateString();
   const currentTime = new Date().toLocaleTimeString();
   const requestInfo = `${currentDate} ${currentTime} - Rota: ${req.path}`;
-
   console.log(requestInfo);
-
   next();
 });
 
-app.set('views', __dirname + '/views');
-app.use(bodyParser.urlencoded({ extended: true }));
-
+// Rotas
 app.use('/placas', placaController.getRouter());
 
 app.use((req, res, next) => {
   res.locals.dadosPersonalizados = { chave: 'valor' };
-  console.log('middleware 0')
+  console.log('middleware 0');
   next();
 });
 
-app.get([
-  '/',
-  '/index'
-  ], (req, res) => {
-      console.log('custom', req.dadosPersonalizados); 
-      placaController.index(req, res)
-});
-
-app.get('/index', (req, res) => res.render('index'));
-app.post('/placas', (req, res) => placaController.area(req, res));
-app.get('/autor', (req, res) => autorController.autor(req, res));
-
-app.get('/perfil' , passport.authenticate('jwt', { session: false, failureRedirect: '/login' }), (req, res) => {
+// Rotas para autenticação
+app.get('/perfil', passport.authenticate('jwt', { session: false, failureRedirect: '/login' }), (req, res) => {
     res.json({'usuario': req.user});
-})
+});
 
 app.get('/login', (req, res) => {
   authController.index(req, res);
@@ -90,15 +83,30 @@ app.post('/logar', (req, res) => {
   authController.logar(req, res);
 });
 
-app.get('*', (req, res, next) => {
-  res.status(404).send('Nao encontrado')
+// Rotas para outras funcionalidades
+app.get(['/', '/index'], (req, res) => {
+  console.log('custom', req.dadosPersonalizados); 
+  placaController.index(req, res);
 });
 
+app.get('/index', (req, res) => res.render('index'));
+
+app.post('/placas', (req, res) => placaController.area(req, res));
+
+app.get('/autor', (req, res) => autorController.index(req, res));
+
+// Middleware para lidar com rotas não correspondidas
+app.use((req, res, next) => {
+  res.status(404).send('Não encontrado');
+});
+
+// Middleware para lidar com erros
 app.use(function (err, req, res, next) {
-  console.error('registrando erro: ', err.stack)
+  console.error('Registrando erro: ', err.stack);
   res.status(500).send('Erro no servidor: ' + err.message);
 });
 
+// Iniciar o servidor
 app.listen(PORT, () => {
   console.log(`Servidor iniciado na porta ${PORT}`);
 });
